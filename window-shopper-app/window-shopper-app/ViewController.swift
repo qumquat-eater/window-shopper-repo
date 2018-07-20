9f3e42fb865e557d778ce938b416f2ac210cd12d
Ferdinand Becker <ferdinand@Ferdinands-iMac.local>
2018-07-20 12:53:49 +0100

    first commit

---
+++ window-shopper-app/window-shopper-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper-app
+//
+//  Created by Ferdinand Becker on 20/07/2018.
+//  Copyright © 2018 Ferdinand Becker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
